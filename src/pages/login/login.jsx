a923592ff45f242b1a9a078cf901cd7879cba6be
Jamoliddin <satimovjamoliddin2000@gmail.com>
2022-05-15 01:25:51 +0500

    add some changes

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,28 +1,28 @@
-import React,{useRef} from 'react';
-import { useAuth } from '../../components/hooks/useAuth';
+import React, { useRef } from "react";
+import { useAuth } from "../../components/hooks/useAuth";
 
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import { useNavigate } from 'react-router-dom';
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { useNavigate } from "react-router-dom";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://t.me/Jamoliddin_Satimov">
-        Your Website
-      </Link>{' '}
+        Developer
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -30,75 +30,68 @@ function Copyright() {
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
-  footer:{
-    marginTop:theme.spacing(8)
+  footer: {
+    marginTop: theme.spacing(8),
   },
 }));
 
-
 export default function SignIn() {
   const classes = useStyles();
-  const {setToken} = useAuth()
-  const navigate = useNavigate()
+  const { setToken } = useAuth();
+  const navigate = useNavigate();
 
-  const emailRef = useRef(null)
-  const passwordRef = useRef(null)
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
 
   function handleOnSubmit(evt) {
-    evt.preventDefault()
-    fetch("https://reqres.in/api/login",{
-            method:"POST",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body: JSON.stringify({
-                email:emailRef.current.value,
-                password:passwordRef.current.value
-            }),
-        })
-        .then(res=>res.json())
-        .then(data=>{
-          if (data.token) {
-            navigate('/')
-            setToken(data)
-          } else {
-            emailRef.current.style.border = "2px solid red"
-            passwordRef.current.style.border = "2px solid red"
-          }
-
-        }).catch(err=>console.log(err))
+    evt.preventDefault();
+    fetch("https://reqres.in/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.token) {
+          navigate("/");
+          setToken(data);
+        } else {
+          emailRef.current.style.border = "2px solid red";
+          passwordRef.current.style.border = "2px solid red";
+        }
+      })
+      .catch((err) => console.log(err));
   }
-  
-
-  
 
   return (
     <Container component="main" maxWidth="xs">
       {/* <CssBaseline /> */}
       <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-        </Avatar>
-        <Typography  variant="h5">
-          Sign in
-        </Typography>
+        <Avatar className={classes.avatar}></Avatar>
+        <Typography variant="h5">Sign in</Typography>
         <form className={classes.form} onSubmit={handleOnSubmit}>
           <TextField
-            variant="outlined"
+            // variant="outlined"
             margin="normal"
             required
             fullWidth
@@ -110,7 +103,7 @@ export default function SignIn() {
             autoFocus
           />
           <TextField
-            variant="outlined"
+            // variant="outlined"
             margin="normal"
             inputRef={passwordRef}
             required
@@ -136,15 +129,9 @@ export default function SignIn() {
           </Button>
           <Grid container>
             <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
+              Password: Any
             </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
+            <Grid item>Email: eve.holt@reqres.in</Grid>
           </Grid>
         </form>
       </div>
